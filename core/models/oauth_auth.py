de7646c53898132a2ed7a4d131394333e571b2aa
Severin Zumbrunn <severin.zumbrunn@abilium.com>
2023-01-09 20:23:07 +0100

    update

--- core/models/oauth_auth.py
+++ core/models/oauth_auth.py
@@ -1,9 +1,31 @@
-
+import requests
+import werkzeug.http
 from odoo import api, models
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
+
+    def _auth_oauth_rpc(self, endpoint, access_token):
+        if self.env['ir.config_parameter'].sudo().get_param('auth_oauth.authorization_header'):
+            response = requests.get(endpoint, headers={'Authorization': 'Bearer %s' % access_token}, timeout=10)
+        else:
+            response = requests.get(endpoint, params={'access_token': access_token}, timeout=10)
+
+        _logger.info(response)
+        if response.ok: # nb: could be a successful failure
+            return response.json()
+
+        auth_challenge = werkzeug.http.parse_www_authenticate_header(
+            response.headers.get('WWW-Authenticate'))
+
+        if auth_challenge.type == 'bearer' and 'error' in auth_challenge:
+            return dict(auth_challenge)
+
+        return {'error': 'invalid_request'}
 
     @api.model
     def _generate_signup_values(self, provider, validation, params):
